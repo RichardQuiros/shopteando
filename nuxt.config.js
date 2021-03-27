e39aabe172561aec6873c334d655078b53e3033e
RichardQuiros <richard.quiroz@uip.edu.pa>
2021-03-27 09:53:14 -0500

    add google verification

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,6 +13,7 @@ export default {
       lang: "en",
     },
     meta: [
+      { name: "google-site-verification" , content: process.env.GOOGLE_VERIFICATION || ""},
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
